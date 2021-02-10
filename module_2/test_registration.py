e1a2336bac30bc60e32623c1d21b91d5490d7a03
Evgeny Egoshin <evgeny.egoshin@kaspersky.com>
2021-02-10 07:54:23 +0300

    Module 2

---
+++ module_2/test_registration.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from sys import argv
+import time
+
+script_name, link = argv
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    first = browser.find_element_by_css_selector ('input.first[required]')
+    first.send_keys('Firstname')
+
+    last = browser.find_element_by_css_selector('input.second[required]')
+    last.send_keys('Lastname')
+
+    email = browser.find_element_by_css_selector('input.third[required]')
+    email.send_keys('email@example.com')
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
