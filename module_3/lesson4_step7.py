2db30020f12c26c311a87572283fbbcfff1e6a22
Evgeny Egoshin <evgeny.egoshin@kaspersky.com>
2021-02-17 19:00:52 +0300

    Module 3 lessons

--- module_3/lesson4_step7.py
+++ module_3/lesson4_step7.py
@@ -1,18 +1,21 @@
 from selenium import webdriver
-from math import log, sin
+from os import path
 from time import sleep
 
+current_dir = path.abspath(path.dirname(__file__))
+file_path = path.join(current_dir, 'test.txt')
+
 link = 'http://suninjuly.github.io/file_input.html'
+browser = webdriver.Chrome()
+browser.get(link)
 
 try:
-    browser = webdriver.Chrome()
-    browser.get(link)
-
     browser.find_element_by_name('firstname').send_keys('First')
     browser.find_element_by_name('lastname').send_keys('Last')
     browser.find_element_by_name('email').send_keys('email@example.com')
 
-
+    browser.find_element_by_name('file').send_keys(file_path)
+    browser.find_element_by_tag_name('button').click()
 
 except:
     sleep(10)
