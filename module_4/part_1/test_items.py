adcc95b0e101e06e652b9272443f036d04516452
Evgeny Egoshin <evgeny.egoshin@kaspersky.com>
2021-02-28 09:08:59 +0300

    Module 4 hw part1

---
+++ module_4/part_1/test_items.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+def test_item_has_addtobasket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
+
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.btn-add-to-basket')))
+
+    assert browser.find_element_by_css_selector('.btn-add-to-basket'), "Add to basket not found"
