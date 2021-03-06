a2b5c30a3894a66272a918a294c18cb9f85423ed
AvatharN <ssecsd@gmail.com>
2021-03-06 08:28:59 +0300

    Module 5 Page Object

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,7 +1,9 @@
+from .pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
 
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        login_link = browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
